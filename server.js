842bf22ca0747e1d4f9db8fadaec00912850ee43
spencercreer <spencercreer@gmail.com>
2021-04-28 21:47:28 -0600

    updated password

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://screer:Slc2018GilAZZ@cluster0.brokn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://screer:Slc2018GilAZ@cluster0.brokn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
